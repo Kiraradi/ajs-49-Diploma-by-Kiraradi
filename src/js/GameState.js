e18acd2d84e02f1dad8968a3f0ad7b52942bcdc8
Anatoliy Katelnitskiy <katel1998@yandex.ru>
2023-02-08 17:37:41 +0300

    add GameState

--- src/js/GameState.js
+++ src/js/GameState.js
@@ -1,6 +1,11 @@
 export default class GameState {
   static from(object) {
+    if (object.moveStatus === 'player') {
+      object.moveStatus = 'enemy';
+    } else if (object.moveStatus === 'enemy') {
+      object.moveStatus = 'player';
+    }
     // TODO: create object
-    return null;
+    return object;
   }
 }
